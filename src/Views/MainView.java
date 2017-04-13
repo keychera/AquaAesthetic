fc9d60a6d899549328c77cea6f0766f3b0112d8f
royyandzakiy <royyan.abdul@gmail.com>
2017-04-13 10:44:59 +0700

    :octocat: MainView

--- src/Views/MainView.java
+++ src/Views/MainView.java
@@ -6,13 +6,19 @@ import javax.swing.JPanel;
 public class MainView extends JFrame {
 	public MainView() {
 		JFrame frame = new JFrame("Play AquaAesthetic");
-		frame.setSize(400,400);
+		int aquariumWidth = 400;
+		int aquariumHeight = 400;
+		frame.setSize(aquariumWidth,aquariumHeight);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
 
 		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new MainView();
 	}
 
 }
