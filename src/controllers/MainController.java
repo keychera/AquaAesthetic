c92039d99caa8549f820a5a1f8da56e11d73183b
keychera <kyogatama@gmail.com>
2017-04-23 15:34:27 +0700

    :pencil: added pause button for testing
    pause button is throwing exception without breaking the game somehow

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -14,92 +14,119 @@ import javax.swing.Timer;
 
 import views.AquariumView;
 
-public class MainController extends JFrame implements ActionListener{
+public class MainController extends JFrame implements ActionListener {
   // app fields
   private static final int FRAMEDELAY = 50;
-  private Timer timer = null;
-  private int aquariumWidth = 400;
-  private int aquariumHeight = 500;
+  private int appWidth = 400;
+  private int appHeight = 500;
   // app swing component
   private JPanel aquariumPanel;
   private JPanel controlPanel;
   private JButton addFishButton;
+  private JButton pauseButton;
   // controller to control
   private FishController fishController;
   // app control
   protected boolean isAppRunning;
   protected boolean isAppPausing;
 
+  private class MainWorker extends SwingWorker<Void, Boolean> {
+    @Override
+    protected Void doInBackground() throws Exception {
+      while (isAppRunning) {
+        if (!isAppPausing) {
+          Thread.sleep(FRAMEDELAY);
+          fishController.perform();
+          publish(true);
+        } else {
+          publish(false);
+        }
+      }
+      return null;
+    }
+
+    @Override
+    protected void process(List<Boolean> calculatedChanges) {
+      for (Boolean isAquariumRunning : calculatedChanges) {
+        if (isAquariumRunning) {
+          aquariumPanel.repaint();
+        }
+      }
+    }
+  }
+
   public MainController() {
-    // Main frame dimension
-    setSize(aquariumWidth, aquariumHeight);
+    initializeMainFrame();
+    initializeAquariumPanel();
+    initializeControlPanel();
+
+    setVisible(true);
+
+    initializeGameLoop();
+  }
+
+  private void initializeMainFrame() {
+    setSize(appWidth, appHeight);
     setResizable(false);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
+  }
 
+  private void initializeAquariumPanel() {
     fishController = new FishController();
     fishController.addFish();
     fishController.addFish();
     fishController.addFish();
     aquariumPanel = new AquariumView(fishController.getFishes());
+  }
 
-    //TODO change this into SwingWorker
-
-    //TODO encapsulate making button for action listener
-    addFishButton = new JButton("add fish");
-    addFishButton.setActionCommand("add fish");
-    addFishButton.addActionListener(this);
-    
+  private void initializeControlPanel() {
     controlPanel = new JPanel();
-    controlPanel.add(addFishButton);
+    addButtonToControlPanel(addFishButton,"add fish");
+    addButtonToControlPanel(pauseButton,"pause");
 
     setLayout(new BorderLayout());
     add(aquariumPanel, BorderLayout.CENTER);
     add(controlPanel, BorderLayout.PAGE_END);
+  }
 
-    setVisible(true);
-    //TODO related to SwingWorker mechanism
+  private void addButtonToControlPanel(JButton button, String message) {
+    addButtonToControlPanel(button, message, message);
+  }
+
+  private void addButtonToControlPanel(JButton button, String message, String label) {
+    button = new JButton(label);
+    button.setActionCommand(message);
+    button.addActionListener(this);
+    controlPanel.add(button);
+  }
+
+
+  private void initializeGameLoop() {
     isAppRunning = true;
     isAppPausing = false;
     runGameLoop();
   }
 
   private void runGameLoop() {
-    SwingWorker<Void, Boolean> mainWorker = new SwingWorker<Void,Boolean>() {
-      @Override
-      protected Void doInBackground() throws Exception {
-        while(isAppRunning) {
-          if (!isAppPausing) {
-            Thread.sleep(10);
-            fishController.perform();
-            publish(true);
-          } else {
-            publish(false);
-          }
-        }
-        return null;
-      }
-
-      @Override
-      protected void process(List<Boolean> calculatedChanges) {
-        for (Boolean isChanged : calculatedChanges) {
-          if (isChanged) {
-            aquariumPanel.repaint();
-            
-          }
-        }
-      }
-    };
+    MainWorker mainWorker = new MainWorker();
     mainWorker.execute();
   }
 
   @Override
   public void actionPerformed(ActionEvent e) {
-    if(e.getActionCommand() == "add fish") {
+    if (e.getActionCommand() == "add fish") {
       Random random = new Random();
-      int bound = aquariumWidth / 10;
-      int randX = bound + random.nextInt(aquariumWidth - (2 * bound));
-      int randY = bound + random.nextInt(aquariumHeight - (2 * bound));
+      int bound = appWidth / 10;
+      int randX = bound + random.nextInt(aquariumPanel.getWidth() - (2 * bound));
+      int randY = bound + random.nextInt(aquariumPanel.getHeight() - (2 * bound));
       fishController.addFish(randX, randY);
+    } else if (e.getActionCommand() == "pause") {
+      isAppPausing = !isAppPausing;
+      if (isAppPausing) {
+        pauseButton.setText("unpause");
+      } else {
+        pauseButton.setText("pause");
+      }
     }
   }
 }
