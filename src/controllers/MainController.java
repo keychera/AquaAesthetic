6e9375031385bc61cff7b5964aecaf760fd108e0
keychera <kyogatama@gmail.com>
2017-04-23 11:02:38 +0700

    :package: prepare for multithreading implementation

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -12,17 +12,18 @@ import javax.swing.Timer;
 
 import views.AquariumView;
 
-public class MainController extends JFrame {
-  JPanel aquariumPanel;
-  JPanel controlPanel;
-  JButton addFishButton;
-
-  FishController fishController;
-
+public class MainController extends JFrame implements ActionListener{
+  // app 
   private static final int FRAMEDELAY = 50;
   private Timer timer = null;
   private int aquariumWidth = 400;
   private int aquariumHeight = 500;
+  // app swing component
+  private JPanel aquariumPanel;
+  private JPanel controlPanel;
+  private JButton addFishButton;
+  // controller to control
+  private FishController fishController;
 
   public MainController() {
     // Main frame dimension
@@ -34,6 +35,7 @@ public class MainController extends JFrame {
     fishController.addFish();
     aquariumPanel = new AquariumView(fishController.getFishes());
 
+    //TODO change this into SwingWorker
     timer = new Timer(FRAMEDELAY, new ActionListener() {
 
       @Override
@@ -44,19 +46,11 @@ public class MainController extends JFrame {
 
     });
 
+    //TODO encapsulate making button for action listener
     addFishButton = new JButton("add fish");
-    addFishButton.addActionListener(new ActionListener() {
-
-      @Override
-      public void actionPerformed(ActionEvent arg0) {
-        Random random = new Random();
-        int bound = aquariumWidth / 10;
-        int randX = bound + random.nextInt(aquariumWidth - (2 * bound));
-        int randY = bound + random.nextInt(aquariumHeight - (2 * bound));
-        fishController.addFish(randX, randY);
-      }
-    });
-
+    addFishButton.setActionCommand("add fish");
+    addFishButton.addActionListener(this);
+    
     controlPanel = new JPanel();
     controlPanel.add(addFishButton);
 
@@ -65,6 +59,18 @@ public class MainController extends JFrame {
     add(controlPanel, BorderLayout.PAGE_END);
 
     setVisible(true);
+    //TODO related to SwingWorker mechanism
     timer.start();
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if(e.getActionCommand() == "add fish") {
+      Random random = new Random();
+      int bound = aquariumWidth / 10;
+      int randX = bound + random.nextInt(aquariumWidth - (2 * bound));
+      int randY = bound + random.nextInt(aquariumHeight - (2 * bound));
+      fishController.addFish(randX, randY);
+    }
   }
 }
