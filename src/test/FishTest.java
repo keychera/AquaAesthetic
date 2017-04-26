9019424568b3956430d1edf06e27d7bbf0dfc6e1
teaolivia <theaolivia.mail@gmail.com>
2017-04-26 12:59:48 +0700

    fixing FoodTest.java and FishTest.java

--- src/test/FishTest.java
+++ src/test/FishTest.java
@@ -1,8 +1,10 @@
-package models;
+package test;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import models.Fish;
 
 public class FishTest {
 	
--- src/test/FoodTest.java
+++ src/test/FoodTest.java
@@ -1,8 +1,10 @@
-package models;
+package test;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import models.Food;
 
 public class FoodTest {
 
